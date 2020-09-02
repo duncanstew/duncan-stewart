8fef674333326c5ce25e9c2246958b71734003ec
duncanstew <dbstewart2@wisc.edu>
2020-09-02 14:59:00 -0500

    rework using tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'custom-off-white': '#f5f8fa',
+        'custom-bright-blue': '#0096cc',
+        'custom-gray': '#696969',
+        'custom-dark-gray': '#3a4145',
+      },
+      fontFamily: {
+        'body': ['Montserrat']
+      }
+      fontSize: {
+        '2.5xl': '1.7rem',
+      },
+      textColor: {
+        'custom-off-white': '#f5f8fa',
+        'custom-bright-blue': '#0096cc',
+        'custom-gray': '#696969',
+        'custom-dark-gray': '#3a4145',
+
+      },
+      spacing: {
+        '15': '3.75rem',
+        '75': '18.75rem',
+        '88': '22rem',
+      },
+      opacity: {
+        '125': '0.125',
+      },
+      minHeight: {
+        '1/4': '25%',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
